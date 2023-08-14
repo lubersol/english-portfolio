b2657b4453a8bc68c57e5f98afc993f19e22e168
lubersol <lubersol@gmail.com>
2023-08-14 13:50:00 +0200

    first commit

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,7 +3,7 @@ import TitleItem from '../components/TitleItem.vue'
 </script>
 
 <template>
-  <div id="projects" class="about sec-pad">
+  <div id="projects" class="projects sec-pad">
     <div class="main-container">
       <TitleItem title="Projects" subtitle text="Here you will find some of the personal and clients projects that I
         created with each project containing its own case study">
