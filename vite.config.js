553ed0495ff03a3d3766c937c130f56ee2aa30b5
lubersol <lubersol@gmail.com>
2023-10-06 13:50:44 +0200

    cleaning errors

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     vue(),
     vueJsx(),
