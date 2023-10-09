89a54893a468ca6f4b4ca199e9d5e57b58dc6f33
lubersol <lubersol@gmail.com>
2023-10-09 17:21:58 +0200

    changing ButtonItem

--- src/components/ButtonItem.vue
+++ src/components/ButtonItem.vue
@@ -1,29 +1,33 @@
 <script setup>
-import { defineProps, computed } from 'vue'
-
+import { defineProps } from 'vue'
 const { text, href } = defineProps(['text', 'href'])
+// import { defineProps, computed } from 'vue'
 
-const absoluteHref = computed(() => {
-  // Utiliza import.meta.env.BASE_URL para obtener la ruta base
-  const BASE_URL = import.meta.env.BASE_URL
-  return `${BASE_URL}${href}`
-})
+// const { text, href } = defineProps(['text', 'href'])
+
+// const absoluteHref = computed(() => {
+//   // Utiliza import.meta.env.BASE_URL para obtener la ruta base
+//   const BASE_URL = import.meta.env.BASE_URL
+//   return `${BASE_URL}${href}`
+// })
 
 // defineProps({
 //   text: {
 //     type: String,
 //     required: true
 //   },
-//   // href: {
-//   //   type: String,
-//   //   required: true
-//   // },
+  // href: {
+  //   type: String,
+  //   required: true
+  // },
 // })
 </script>
 
+
+
 <template>
   <div class="home-hero__cta">
-    <a :href="absoluteHref" class="btn btn--bg">{{ text }}</a>
+    <a :href="`${import.meta.env.BASE_URL}${href}`" class="btn btn--bg">{{ text }}</a>
   </div>
 </template>
 
