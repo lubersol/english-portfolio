1dbde2e8ff0a0652226bf97f46ce7a2a0edad43b
lubersol <lubersol@gmail.com>
2023-10-09 11:32:04 +0200

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/portfolio',
+  base: '/portfolio',
   build: {
     outDir: 'dist',
   },
